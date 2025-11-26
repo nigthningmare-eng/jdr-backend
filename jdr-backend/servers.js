df2b466885cd71cf73766b236d64baa252753be7
nigthningmare-eng <nigthningmare@gmail.com>
2025-11-26 11:24:44 +0400

    Update servers.js

--- jdr-backend/servers.js
+++ jdr-backend/servers.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
+const fetch = require('node-fetch');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -70,6 +71,7 @@ app.post('/api/generate/scene', (req, res) => {
 app.listen(port, () => {
   console.log(`JDR API en ligne sur http://localhost:${port}`);
 });
+
 
 
 
